36816716095c33817caeb69754f1b786e0d375b9
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2010-03-04 16:08:42 +0100

    use new Spline methods to marshal/demarshal

--- src/spline.cpp
+++ src/spline.cpp
@@ -12,45 +12,17 @@ Spline::Spline(::base::geometry::SplineBase const& source)
     SISLCurve const* curve = source.getSISLCurve();
     if (curve)
     {
-        int vertex_count = curve->in;
-        curve_order      = curve->ik;
         kind             = static_cast<SplineType>(curve->ikind);
-
-        knots.resize(vertex_count + curve_order);
-
-        memcpy(&knots[0], &(curve->et[0]), sizeof(double) * knots.size());
-
-        if (kind % 2 == 0) // rational curve
-        {
-            vertices.resize(vertex_count * (dimension + 1));
-            memcpy(&vertices[0], &(curve->rcoef[0]), sizeof(double) * vertices.size());
-        }
-        else
-        {
-            vertices.resize(vertex_count * dimension);
-            memcpy(&vertices[0], &(curve->ecoef[0]), sizeof(double) * vertices.size());
-        }
+        knots            = source.getKnots();
+        vertices         = source.getCoordinates();
     }
 }
 
 Spline::operator ::base::geometry::SplineBase() const
 {
+    base::geometry::SplineBase result(dimension, geometric_resolution, curve_order);
     if (!knots.empty()) // we have a SISL curve definition
-    {
-        int vertex_count;
-        if (kind % 2 == 0) // rational curve;
-            vertex_count = vertices.size() / (dimension + 1);
-        else
-            vertex_count = vertices.size() / dimension;
-
-        SISLCurve* curve = newCurve(vertex_count,
-                curve_order, const_cast<double*>(&knots[0]), const_cast<double*>(&vertices[0]), kind, dimension, 1);
-        curve->cuopen = 1;
-        return base::geometry::SplineBase(geometric_resolution, curve);
-    }
-    else
-    {
-        return base::geometry::SplineBase(dimension, geometric_resolution, curve_order);
-    }
+        result.reset(vertices, knots, kind);
+    return result;
 }
 
