b3173f3103b9f9a7096a1fa3195e6307800a7ae0
Sylvain Joyeux <sylvain.joyeux@dfki.de>
2010-08-19 12:43:45 +0200

    use the new Typelib.convert_to_ruby in scripts/typelib.rb

--- scripts/typelib.rb
+++ scripts/typelib.rb
@@ -1,8 +1,8 @@
-Typelib.specialize_model '/base/Time' do
-    def to_ruby(value)
-        Typelib::DynamicWrapper(Time.at(value.seconds, value.microseconds), value)
-    end
+# If we get a /base/Time, convert it to Ruby's Time class
+Typelib.convert_to_ruby '/base/Time' do |value|
+    Time.at(value.seconds, value.microseconds)
 end
+# Tell Typelib that Time instances can be converted into /base/Time values
 Typelib.convert_from_ruby Time, '/base/Time' do |value, typelib_type|
     result = typelib_type.new
     result.seconds      = value.tv_sec
@@ -13,15 +13,11 @@ end
 
 begin
     require 'eigen'
-    Typelib.specialize_model '/wrappers/Vector3' do
-        def to_ruby(value)
-            Typelib::DynamicWrapper(Eigen::Vector3.new(*value.data.to_a), value)
-        end
+    Typelib.convert_to_ruby '/wrappers/Vector3' do |value|
+        Eigen::Vector3.new(*value.data.to_a)
     end
-    Typelib.specialize_model '/wrappers/Quaternion' do
-        def to_ruby(value)
-            Typelib::DynamicWrapper(Eigen::Quaternion.new(value.re, *value.im.to_a), value)
-        end
+    Typelib.convert_to_ruby '/wrappers/Quaternion' do |value|
+        Eigen::Quaternion.new(value.re, *value.im.to_a)
     end
 rescue LoadError
     STDERR.puts "The Ruby Eigen library is not present, I am not providing extensions for the base geometry types"
