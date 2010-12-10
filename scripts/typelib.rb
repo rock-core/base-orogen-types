9f21a4874de92adf3677dc736db09d7f71cd0cfc
Sylvain Joyeux <sylvain.joyeux@dfki.de>
2010-12-10 17:53:32 +0100

    typelib: fix /base/Time to Ruby Time convertion
    there's no second field anymore

--- scripts/typelib.rb
+++ scripts/typelib.rb
@@ -1,6 +1,8 @@
 # If we get a /base/Time, convert it to Ruby's Time class
 Typelib.convert_to_ruby '/base/Time' do |value|
-    Time.at(value.seconds, value.microseconds)
+    microseconds = value.microseconds
+    seconds = microseconds / 1_000_000
+    Time.at(seconds, microseconds % 1_000_000)
 end
 # Tell Typelib that Time instances can be converted into /base/Time values
 Typelib.convert_from_ruby Time, '/base/Time' do |value, typelib_type|
