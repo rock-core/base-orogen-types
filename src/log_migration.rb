bf403bfe943048c1e6d8f53940b8ae36da3ec5f5
Alexander Duda <Alexander.Duda@dfki.de>
2011-09-20 17:36:41 +0200

    log_immigration SonarScan: fix angle conversion

--- src/log_migration.rb
+++ src/log_migration.rb
@@ -4,7 +4,7 @@ LogTools::Converter.register "converter for base type sonar scan", time,Orocos.r
     conversion "/base/samples/SonarScan","/base/samples/SonarBeam" do |dst,src|
         deep_cast(dst.beam,src.scanData)
         dst.time = src.time
-        dst.bearing = src.angle - Math::PI
+        dst.bearing = src.angle > Math::PI ? src.angle-2*Math::PI : src.angle
         dst.sampling_interval = src.time_beetween_bins
         dst.beamwidth_vertical = 35/180* Math::PI 
         dst.beamwidth_horizontal = 3/180* Math::PI 
