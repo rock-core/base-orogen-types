d9fd8e8a39939d43d81d54f32f6c8886b71d059b
Alexander Duda <Alexander.Duda@dfki.de>
2011-09-20 16:25:07 +0200

    change base type SonarScan to SonarBeam + add conversion

--- lib/base/immigration.rb
+++
@@ -1,9 +0,0 @@
-require 'orocos/log/converter'
-require 'orocos'
-
-Orocos.initialize
-include Orocos::Log
-
-#load all conversions for base types 
-require_dir File.join(File.dirname(__FILE__),'immigration')
-
---
+++ src/log_migration.rb
@@ -0,0 +1,13 @@
+#this conversions are used if the sample is older than time
+time = Time.utc 2011,9,19
+LogTools::Converter.register "converter for base type sonar scan", time,Orocos.registry do
+    conversion "/base/samples/SonarScan","/base/samples/SonarBeam" do |dst,src|
+        deep_cast(dst.beam,src.scanData)
+        dst.time = src.time
+        dst.bearing = src.angle - Math::PI
+        dst.sampling_interval = src.time_beetween_bins
+        dst.beamwidth_vertical = 35/180* Math::PI 
+        dst.beamwidth_horizontal = 3/180* Math::PI 
+        dst.speed_of_sound = 1500
+    end
+end
