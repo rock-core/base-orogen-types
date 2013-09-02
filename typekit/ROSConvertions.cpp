d28f210312866c33bc2e29be83103f26e39aa348
Thomas Roehr <thomas.roehr@dfki.de>
2013-09-02 17:27:39 +0200

    Add ROS mappings for joint state and pose

--- typekit/ROSConvertions.cpp
+++ typekit/ROSConvertions.cpp
@@ -106,3 +106,100 @@ void ros_convertions::fromROS( ::RTT::extras::ReadOnlyPointer< ::base::samples::
     value.reset(frame);
 }
 
+void ros_convertions::toROS( sensor_msgs::JointState& ros, ::base::samples::Joints  const& value )
+{
+    toROS(ros.header.stamp, value.time);
+
+    ros.name.resize(value.names.size());
+    ros.position.resize(value.elements.size());
+    ros.velocity.resize(value.elements.size());
+    ros.effort.resize(value.elements.size());
+
+    ros.name =  value.names;
+
+    for(size_t i = 0; i < value.elements.size(); ++i)
+    {
+        ros.position.at(i) =  value.elements.at(i).position;
+        ros.velocity.at(i) =  value.elements.at(i).speed;
+        ros.effort.at(i)   =  value.elements.at(i).effort;
+    }
+
+}
+
+void ros_convertions::fromROS( ::base::samples::Joints& value, sensor_msgs::JointState const& ros )
+{
+    fromROS(value.time, ros.header.stamp);
+
+    value.names.resize(ros.name.size());
+    value.names = ros.name;
+
+    // fields are optional in the ROS type and associated arrays
+    // "should have the same size or be empty"
+    size_t joint_number = std::max( ros.position.size(), ros.effort.size());
+    joint_number = std::max(joint_number, ros.velocity.size());
+
+    value.elements.resize(joint_number);
+
+    for(size_t i = 0; i < joint_number; ++i)
+    {
+        if(!ros.position.empty())
+        {
+            value.elements.at(i).position = ros.position.at(i);
+        }
+
+        if(!ros.velocity.empty())
+        {
+            value.elements.at(i).speed = ros.velocity.at(i);
+        }
+
+        if(!ros.effort.empty())
+        {
+            value.elements.at(i).effort = ros.effort.at(i);
+        }
+    }
+}
+
+void ros_convertions::toROS( geometry_msgs::Pose& ros, ::base::Pose  const& value )
+{
+        ros.position.x = value.position.x();
+        ros.position.y = value.position.y();
+        ros.position.z = value.position.z();
+        ros.orientation.x = value.orientation.x();
+        ros.orientation.y = value.orientation.y();
+        ros.orientation.z = value.orientation.z();
+        ros.orientation.w = value.orientation.w();
+}
+
+void ros_convertions::fromROS( ::base::Pose& value, geometry_msgs::Pose const& ros )
+{
+    value.position.x() = ros.position.x;
+    value.position.y() = ros.position.y;
+    value.position.z() = ros.position.z;
+    value.orientation.x() = ros.orientation.x;
+    value.orientation.y() = ros.orientation.y;
+    value.orientation.z() = ros.orientation.z;
+    value.orientation.w() = ros.orientation.w;
+}
+
+void ros_convertions::toROS( geometry_msgs::Pose& ros, ::base::Pose_m const& value )
+{
+    ros.position.x = value.position.data[0];
+    ros.position.y = value.position.data[1];;
+    ros.position.z = value.position.data[2];
+    ros.orientation.x = value.orientation.im[0];
+    ros.orientation.y = value.orientation.im[1];
+    ros.orientation.z = value.orientation.im[2];
+    ros.orientation.w = value.orientation.re;
+}
+
+void ros_convertions::fromROS( ::base::Pose_m& value, geometry_msgs::Pose const& ros )
+{
+    value.position.data[0] = ros.position.x;
+    value.position.data[1] = ros.position.y;
+    value.position.data[2] = ros.position.z;
+    value.orientation.im[0] = ros.orientation.x;
+    value.orientation.im[1] = ros.orientation.y;
+    value.orientation.im[2] = ros.orientation.z;
+    value.orientation.re = ros.orientation.w;
+}
+
--- typekit/ROSConvertions.hpp
+++ typekit/ROSConvertions.hpp
@@ -9,9 +9,9 @@
 
 
 #include <std_msgs/Time.h>
-
 #include <sensor_msgs/Image.h>
-
+#include <geometry_msgs/Pose.h>
+#include <sensor_msgs/JointState.h>
 
 namespace ros_convertions {
     /** Converted types: */
@@ -22,10 +22,18 @@ namespace ros_convertions {
     void toROS( sensor_msgs::Image& ros, ::RTT::extras::ReadOnlyPointer< ::base::samples::frame::Frame > const& value );
     void fromROS( ::RTT::extras::ReadOnlyPointer< ::base::samples::frame::Frame >& value, sensor_msgs::Image const& ros );
     
-    
     void toROS( sensor_msgs::Image& ros, ::base::samples::frame::Frame const& value );
     void fromROS( ::base::samples::frame::Frame& value, sensor_msgs::Image const& ros );
+
+    void toROS( geometry_msgs::Pose& ros, ::base::Pose const& value );
+    void fromROS( ::base::Pose& value, geometry_msgs::Pose const& ros );
+
+    void toROS( geometry_msgs::Pose& ros, ::base::Pose_m const& value );
+    void fromROS( ::base::Pose_m& value, geometry_msgs::Pose const& ros );
     
+    void toROS( sensor_msgs::JointState& ros, ::base::samples::Joints const& value );
+    void fromROS( ::base::samples::Joints& value, sensor_msgs::JointState const& ros );
+
 }
 
 #endif
